72a3513c6f3d750ee8855e6524e8349999b126ca
SLevendeev <stanislav-levendeev@mail.ru>
2025-07-06 15:29:17 +0200

    Fix image color conversion assignment in read_image function

--- src/annotation/image_annotator.py
+++ src/annotation/image_annotator.py
@@ -4,6 +4,6 @@ import cv2
 def read_image(image_path: str):
     """Read an image from a given path."""
     image = cv2.imread(image_path)
-    cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     img_height, img_width, _ = image.shape
     return image, img_width, img_height
